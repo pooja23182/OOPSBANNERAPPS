753ec8daeeb107a3b6f3c5670eddba11cccc821c
pooja23182 <pooja23vijayakumar@gmail.com>
2026-02-14 11:41:43 +0530

    oopsuc2

---
+++ oopsuc2.java
@@ -0,0 +1,17 @@
+public class oopsuc2 {
+   
+    public static void main(String[] args) {
+        // Each letter block [ "XXXXXX  " ] is exactly 8 characters wide.
+        // This ensures vertical alignment regardless of the letter shape.
+
+        System.out.println("  ***** " + "  ***** " + " ****** " + "  ***** ");
+        System.out.println(" **  ** " + " **  ** " + " **  ** " + " ** ");
+        System.out.println(" **  ** " + " **  ** " + " **  ** " + " ** ");
+        System.out.println(" **  ** " + " **  ** " + " ****** " + "  **** ");
+        System.out.println(" **  ** " + " **  ** " + " ** " + "         ** ");
+        System.out.println(" **  ** " + " **  ** " + " ** " + "         ** ");
+        System.out.println("  ***** " + "  ***** " + " ** " + "     ***** ");
+    }
+}
+    
+
