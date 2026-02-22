e9f201374277568fe6ef1fa45bd5128c3b669b08
pooja23182 <pooja23vijayakumar@gmail.com>
2026-02-22 20:54:37 +0530

    uc4

---
+++ uc3stringjoin.java
@@ -0,0 +1,20 @@
+public class uc3stringjoin {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ", " ***** ", " ***** ", " ***** ", " ***** "));
+        System.out.println(String.join(" ", "*     *", "*     *", "*     *", "*      "));
+        System.out.println(String.join(" ", "*     *", "*     *", "*     *", "*      "));
+        System.out.println(String.join(" ", "*     *", "*     *", " ***** ", " ***** "));
+        System.out.println(String.join(" ", "*     *", "*     *", "*      ", "      *"));
+        System.out.println(String.join(" ", "*     *", "*     *", "*      ", "      *"));
+        System.out.println(String.join(" ", " ***** ", " ***** ", "*      ", " ***** "));
+    }
+}
+
+     
+
+
+    
+    
+
